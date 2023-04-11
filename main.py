8454189651b26c2bcb11dfd2c3d8ef881eefc982
Idriss Dassi Kamdoum <87118993+prims06@users.noreply.github.com>
2023-04-11 14:19:06 +0100

    game over lock screen

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ class MainWidget(Widget):
     SHIP_BASE_Y = 0.04
     ship_coordinates = [(0,0),(0,0),(0,0)]
 
+    state_game_over = False
+
     def is_desktop(self):
         if platform in ('linux', 'win', 'macosx'):
             return True
@@ -215,19 +217,21 @@ class MainWidget(Widget):
         self.update_horizontal_lines()
         self.update_tiles()
         self.update_ship()
-        speed_x = self.current_speed_x * self.width / 100
-        speed_y = self.SPEED * self.height/100
-        self.current_offset_y += speed_y * time_factor
+        if not self.state_game_over:
+            speed_x = self.current_speed_x * self.width / 100
+            speed_y = self.SPEED * self.height/100
+            self.current_offset_y += speed_y * time_factor
 
-        spacing_y = self.H_LINES_SPACING * self.height
-        if self.current_offset_y >= spacing_y:
-            self.current_offset_y -= spacing_y
-            self.current_y_loop += 1
-            self.generate_tiles_coordinates()
-        self.current_offset_x += speed_x * time_factor
+            spacing_y = self.H_LINES_SPACING * self.height
+            while self.current_offset_y >= spacing_y:
+                self.current_offset_y -= spacing_y
+                self.current_y_loop += 1
+                self.generate_tiles_coordinates()
+            self.current_offset_x += speed_x * time_factor
 
-        if not self.check_ship_collisions():
-            print("GAME OVER")
+            if not self.check_ship_collisions():
+                self.state_game_over = True
+                print("GAME OVER")
 
 
 class GalaxyApp(App):
